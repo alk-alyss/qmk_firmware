08a7bf62249f001781727a1f7468354726dca831
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2024-02-09 12:39:34 +0200

    add esc on gaming layer

--- keyboards/keycapsss/kimiko/keymaps/alk-alyss/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/alk-alyss/keymap.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* GAMING
  * ,-----------------------------------------.                    ,----------------------------------------------.
- * |      |  1   |  2   |  3   |  4   |  5   |                    |  6   |  7   |  8   |  9   |  0   | BASE_LAYER|
+ * |  ESC |  1   |  2   |  3   |  4   |  5   |                    |  6   |  7   |  8   |  9   |  0   | BASE_LAYER|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+-----------|
  * |  T   | TAB  |  Q   |  W   |  E   |  R   |                    |  Y   |  U   |  UP  |  O   |  P   |           |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+-----------|
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_GAMING] = LAYOUT(
-    _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, DF(_BASE),
+     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, DF(_BASE),
        KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                      KC_Y,    KC_U,   KC_UP,    KC_O,    KC_P, XXXXXXX,
        KC_I, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,                      KC_G, KC_LEFT, KC_DOWN,KC_RIGHT, XXXXXXX, XXXXXXX,
        KC_M, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V, KC_MPLY, KC_MUTE,    KC_H,    KC_J,    KC_K,    KC_L,    KC_N, XXXXXXX,
